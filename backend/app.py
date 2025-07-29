250ef524653fe24df2a166d139f13ce075308d93
Chijindum <chijindumndukife05@gmail.com>
2025-07-29 19:57:48 +0100

    final year

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+# backend/app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello from your Flask backend!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
